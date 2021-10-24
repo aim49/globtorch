4ca64052ce9229aeaf91e959d824fe44ac089171
aim49 <37363378+aim49@users.noreply.github.com>
2021-10-24 03:41:45 +0200

    page updates
    page updates

---
+++ js/main.js
@@ -0,0 +1,16 @@
+jQuery( document ).ready(function( $ ) {
+
+
+	"use strict";
+
+        // Page loading animation
+
+        $("#preloader").animate({
+            'opacity': '0'
+        }, 600, function(){
+            setTimeout(function(){
+                $("#preloader").css("visibility", "hidden").fadeOut();
+            }, 300);
+        }); 
+ 
+});
